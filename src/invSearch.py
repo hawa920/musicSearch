52abe4d943a13c9cff88dcc20e7390664f1588a0
Howard19960920 <hower.song@gmail.com>
2018-06-10 11:40:54 +0800

    add correct rate in classification

--- src/invSearch.py
+++ src/invSearch.py
@@ -4,6 +4,7 @@
 import os
 import re
 import json
+import time
 import hashlib
 import featExtractor
 import numpy as np
@@ -27,16 +28,18 @@ if __name__ == "__main__":
 
     invidx = {}
     # build inverted index
+    start_time = time.time()
     for key, val in features.items():
         for fpt in val:
             try:
                 invidx[fpt].append(key)
             except:
                 invidx[fpt] = [key]
+    
     # remove redundent
     for key, val in invidx.items():
         invidx[key] = list(set(invidx[key]))
-
+    print('Time to build inverted index {0}'.format(time.time() - start_time))
     # inverted index search
     classfy = []
     items = os.listdir(TEST_CLIP_DIR)
@@ -44,7 +47,7 @@ if __name__ == "__main__":
         classfy.clear()
         sound = AudioSegment.from_file(TEST_CLIP_DIR + item).set_channels(1).set_frame_rate(SAMPLING_RATE)
         samples = sound.get_array_of_samples()
-        thisFeats = list(featExtractor.fingerprint(samples))
+        thisFeats = list(featExtractor.fingerprint(samples)) # this is the bottle neck
         thisFeats = list(set([x[0] for x in thisFeats]))
         # iterate fingerprints (a.k.a. the keywords)
         for keyword in thisFeats:
@@ -56,6 +59,10 @@ if __name__ == "__main__":
         resultlist = [[key, cnt] for key, cnt in Counter(classfy).items()]
         try:
             result = max(resultlist, key = itemgetter(1)) # 0:key, 1:cnt
-            print('Classified result: {0} --> {1}\tRate:{2}'.format(item, result, result[1] / len(thisFeats)))
+            correct_rate = result[1] / len(thisFeats)
+            if correct_rate >= 0.009:
+                print('{0}\t{1}\tRate:{2}'.format(item, 'https://www.youtube.com/watch?v=' + result[0], correct_rate))
+            else:
+                raise Exception
         except:
-            print('Music: {0} not found'.format(item))
+            print('{0}\tNot Found'.format(item))
